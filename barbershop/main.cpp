eb6b132eafbb6d83c1a8c5542247705f9e2df856
Jim Crawford <jim@goombas.org>
2013-06-22 17:26:01 -0700

    - Texture loading. - Dog rendering.

--- barbershop/main.cpp
+++ barbershop/main.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <vector>
 #include "glwrap.h"
+#include "listener.h"
+#include "vect2.h"
+
+float player_color[4][4] =
+{
+  {1.0, 0.5, 1.0, 1.0},
+  {0.75, 1.0, 0.75, 1.0},
+  {0.5, 1.0, 1.0, 1.0},
+  {1.0, 1.0, 0.5, 1.0}
+};
 
 void drawBarberPole(int x, int y, int width, int height, float stripe_height,
   float stripe_gap, float stripe_slope, float stripe_offset,
-  float target_offset)
+  float target_offset, int player)
 {
-  //stripe_offset = 0.5f;
-
+  glBindTexture(GL_TEXTURE_2D, 0);
+  
   glScissor(x, 768-height-y, width, height);
   glEnable(GL_SCISSOR_TEST);
 
@@ -32,19 +45,93 @@ void drawBarberPole(int x, int y, int width, int height, float stripe_height,
   }
 
   float toffset = target_offset*wavelength;
-  glColor4f(0.1f, 0.1f, 0.6f, 0.5f);
+  glColor4f(player_color[player][0]*0.6f, player_color[player][1]*0.6f, player_color[player][2]*0.6f, 0.75f);
   glVertex2f(x, y+height/2+wavelength/2-stripe_height+toffset);
   glVertex2f(x, y+height/2+wavelength/2+toffset);
-  glColor4f(0.2f, 0.2f, 1.0f, 0.5f);
+  glColor4f(player_color[player][0], player_color[player][1], player_color[player][2], 0.75f);
   glVertex2f(x+width, y+height/2+wavelength/2-width*stripe_slope+toffset);
   glVertex2f(x+width, y+height/2+wavelength/2-stripe_height-width*stripe_slope+toffset);
 
+  glEnd();
+
+  glDisable(GL_SCISSOR_TEST);
+}
+
+int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width,
+  unsigned long& image_height, const unsigned char* in_png, size_t in_size,
+  bool convert_to_rgba32 = true);
+
+int loadTexture(char *png_filename)
+{
+  FILE *in = fopen(png_filename, "rb");
+  fseek(in, 0, SEEK_END);
+  int size = ftell(in);
+  fseek(in, 0, SEEK_SET);
+  void *data = malloc(size);
+  fread(data, size, 1, in);
+  fclose(in);
+
+  std::vector<unsigned char> rgba;
+  unsigned long width, height;
+  decodePNG(rgba, width, height, (const unsigned char *)data, size);
+  free(data);
+
+  int id;
+
+  glGenTextures(1, (GLuint *)&id);
+	glBindTexture(GL_TEXTURE_2D, id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+    &(rgba.front()));
+
+  return id;
+}
+
+void glVertexVect2(vect2f p)
+{
+  glVertex2f(p.x, p.y);
+}
+
+void drawDog(point p, float angle, int dogfur, int dogoverlay, int player)
+{
+  vect2f xside(128, 0), yside(0, 128);
+  xside = xside.rotate(angle);
+  yside = yside.rotate(angle);
+
+  glBindTexture(GL_TEXTURE_2D, dogfur);
+  glBegin(GL_QUADS);
+  glColor4f(player_color[player][0], player_color[player][1], player_color[player][2], 1);
+  glTexCoord2f(0, 0);
+  glVertexVect2(p-xside/2-yside/2);
+  glTexCoord2f(1, 0);
+  glVertexVect2(p+xside/2-yside/2);
+  glTexCoord2f(1, 1);
+  glVertexVect2(p+xside/2+yside/2);
+  glTexCoord2f(0, 1);
+  glVertexVect2(p-xside/2+yside/2);
+  glEnd();
+
+  glBindTexture(GL_TEXTURE_2D, dogoverlay);
+  glBegin(GL_QUADS);
+  glColor4f(1, 1, 1, 1);
+  glTexCoord2f(0, 0);
+  glVertexVect2(p-xside/2-yside/2);
+  glTexCoord2f(1, 0);
+  glVertexVect2(p+xside/2-yside/2);
+  glTexCoord2f(1, 1);
+  glVertexVect2(p+xside/2+yside/2);
+  glTexCoord2f(0, 1);
+  glVertexVect2(p-xside/2+yside/2);
   glEnd();
 }
 
 int main(int argc, char **argv)
 {
+  //fakeListenerInit();
+
   int width = 1024, height = 768;
+  
   createglwindow("Test", width, height, 32, false);
  
   glEnable(GL_TEXTURE_2D);
@@ -57,8 +144,16 @@ int main(int argc, char **argv)
 
   float offset = 0;
 
+  int dogfur = loadTexture("dogfur.png");
+  int dogoverlay = loadTexture("dogoverlay.png");
+
+  float position[4] = {50, 50, 50, 50};
+
+  int time = 0;
   for(;;)
   {
+    time++;
+
     MSG msg;
     if(PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
     { 
@@ -78,9 +173,29 @@ int main(int argc, char **argv)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    drawBarberPole(50, 50, 150, 500, 100, 100, 0.7, offset, 0.25);
-    offset += 0.01f;
+/*    float freq[4], amp[4];
+    fakeListen(freq, amp);
+    
+    while(freq >= 220) freq /= 2;
+    while(freq <  110) freq *= 2;
+
+    printf("%f %f", freq, amp);*/
+
+    offset = 55.0/110.0;
+
+    for(int i=0; i<4; i++) position[i] += sin((float)i);
+
+    drawBarberPole( 50, 50, 150, 500, 100, 100, 0.7, offset, 0.25, 0);
+    drawBarberPole(250, 50, 150, 500, 100, 100, 0.7, offset, 0.25, 1);
+    drawBarberPole(450, 50, 150, 500, 100, 100, 0.7, offset, 0.25, 2);
+    drawBarberPole(650, 50, 150, 500, 100, 100, 0.7, offset, 0.25, 3);
+    //offset += 0.01f;
     if(offset > 1) offset -= 1;
+
+    drawDog(vect2f(position[0], 520), sin(time/10.0)/3, dogfur, dogoverlay, 0);
+    drawDog(vect2f(position[1], 560), sin(time/8.0)/3, dogfur, dogoverlay, 1);
+    drawDog(vect2f(position[2], 600), sin(time/12.0)/3, dogfur, dogoverlay, 2);
+    drawDog(vect2f(position[3], 640), sin(time/11.0)/3, dogfur, dogoverlay, 3);
 
     glFinish();
     SwapBuffers(hdc);
---
+++ barbershop/vect2.h
@@ -0,0 +1,101 @@
+/** \file vect2.h
+    \brief Provides a 2d vector template and several classes which use it. */
+#ifndef _VECT2_H
+#define _VECT2_H
+
+#include <cmath>
+#include <iostream>
+//#include "list.h"
+//#include "misc.h"
+
+/// 2D Vector class.
+template <typename T>
+class vect2
+{ public:
+    vect2() { x=y=0; } ///< x and y components 0
+    vect2(T x_, T y_) { x=x_, y=y_; }
+
+    /// Dot product.
+    T operator*(const vect2<T> &v) const { return x*v.x + y*v.y; }
+
+    /// Vector addition.
+    vect2<T> operator+(const vect2<T> &v) const
+    { return vect2(x+v.x, y+v.y);
+    }
+    /// Vector subtraction.
+    vect2<T> operator-(const vect2<T> &v) const
+    { return vect2(x-v.x, y-v.y);
+    }
+    /// Vector multiply by scalar.
+    vect2<T> operator*(const T &s) const { return vect2<T>(x*s, y*s); }
+    /// Vector divide by scalar.
+    vect2<T> operator/(const T &s) const { return vect2<T>(x/s, y/s); }
+    /// Vector negate.
+    vect2<T> operator-() const { return vect2<T>(-x, -y); }
+
+    vect2<T> operator+=(const vect2<T> &v) { x+=v.x, y+=v.y; return *this; }
+    vect2<T> operator-=(const vect2<T> &v) { x-=v.x, y-=v.y; return *this; }
+    vect2<T> operator*=(const T        &s) { x*=s,   y*=s;   return *this; }
+    vect2<T> operator/=(const T        &s) { x/=s,   y/=s;   return *this; }
+
+    bool operator==(const vect2<T> &v) const { return x==v.x && y==v.y; }
+    bool operator!=(const vect2<T> &v) const { return x!=v.x || y!=v.y; }
+    
+    /// Check if above or to the left.
+    bool operator< (const vect2<T> &v) const
+    { return y<v.y || y==v.y && x<v.x;
+    }
+    /// Check if below or to the right.
+    bool operator> (const vect2<T> &v) const
+    { return y>v.y || y==v.y && x>v.x;
+    }
+
+    /** \brief Assuming the vectors are points on a 2d plane, the square of
+        the distance between them. */
+    T distance_squared(const vect2<T> &v) const
+    { return (v.x-x)*(v.x-x) + (v.y-y)*(v.y-y);
+    }
+    /** \brief Assuming the vectors are points on a 2d plane, the distance
+        between them. */
+    T distance(const vect2<T> &v) const 
+    { return sqrt(distance_squared(v));
+    }
+    /// Length of vector
+    double magnitude() const { return distance(vect2<T>(0, 0)); }
+
+    /// Keep direction, but set length to 1.
+    vect2<T> normalize() const { return *this/magnitude(); }
+    /// One of the vectors perpendicular to this.
+    vect2<T> perpendicular() const { return vect2<T>(-y, x); }
+
+    vect2<T> rotate(float angle) const
+    {
+      return vect2<T>(
+        x*cos(angle)-y*sin(angle), y*cos(angle)+x*sin(angle));
+    }
+
+    T x, y; ///< Actual data
+};
+
+//@{
+/// Vect2 of type float
+typedef vect2<float> vect2f;
+typedef vect2f point;
+typedef vect2f delta;
+typedef vect2f normal;
+typedef vect2f size;
+//@}
+
+//@{
+/// Vect2 of type int
+typedef vect2<int> vect2i;
+typedef vect2i pointi;
+typedef vect2i sizei;
+//@}
+
+/// Convert vect2 to vect2<int>
+inline vect2i v2i(vect2f v) { return vect2i(int   (v.x), int   (v.y)); }
+/// Convert vect2 to vect2<float>
+inline vect2f v2f(vect2f v) { return vect2f(float(v.x), float(v.y)); }
+
+#endif
